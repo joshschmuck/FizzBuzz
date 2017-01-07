d2bee46689cba5a85b23b3c3216988604bff1cf3
Joshua Schmuck <josh.schmuck@gmail.com>
2017-01-06 18:31:34 -0800

    initial commit

---
+++ src/FizzBuzz/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 101; i++)
+            {
+                if(i % 3 ==0 && i % 7 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }else
+                {
+                    Console.WriteLine(i);
+                }
+                //if (i % 3 == 0)
+                //{
+                //    Console.WriteLine("Fizz");
+                //}
+                //if (i % 7 == 0)
+                //{
+                //    //string buzz = i.Replace(i, "buzz");
+                //    Console.WriteLine("Buzz");
+                //}
+
+                //Figuring out how to possibly use .Replace instead of an if statement
+            }
+             Console.ReadLine();
+        }
+    }
+}
